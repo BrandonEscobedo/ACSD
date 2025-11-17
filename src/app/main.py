64a95f3c8ddd39427694bb4f7760f4b4a822da66
Brandon Escobedo <brandonescobedo1234@gmail.com>
2025-11-17 15:13:07 -0600

    arquitectura

--- src/app/main.py
+++ src/app/main.py
@@ -1,19 +1,3 @@
-from services.linea_transportista_service import LineaTransportistaServicio
-from dataclasses import asdict
-
-def main():
-    servicio = LineaTransportistaServicio('data/lineas_transportistas.json')
-    lineas = servicio.listar_lineas()
-    for linea in lineas:
-        print(asdict(linea))
-
-if __name__ == "__main__":
-    main()
-"""
-Simulación de Contenedores: Buque → Piso → Patio
-Integración SimPy + Streamlit con animación visual
-"""
-
 import streamlit as st
 import simpy
 import time
